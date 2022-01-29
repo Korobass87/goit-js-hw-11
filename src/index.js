0c5d2a22d112998e2691c6917f654daf6939c6e8
Alexander Korobov <korobass1987@yandex.ru>
2022-01-29 20:05:00 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,32 +1,26 @@
 import './sass/main.scss';
 import SimpleLightbox from "simplelightbox"
-
+import Notiflix from 'notiflix';
+import throttle from "lodash.throttle";
+Notiflix.Notify.init({
+  
+  position: 'right-top',
+  distance: '10px',
+  opacity: 1,
+  // ...
+});
 import 'simplelightbox/dist/simple-lightbox.min.css'
+
+
 const axios = require('axios');
 const form = document.querySelector(".search-form")
 const gallery = document.querySelector(".gallery")
-
 const loadMoreBTN = document.querySelector(".load-more")
-loadMoreBTN.classList.add("visually-hidden")
+// loadMoreBTN.classList.add("visually-hidden")
 let currentPage = 1
 let currentSeach = ""
 
-
-
-form.addEventListener("submit", getSeach)
-loadMoreBTN.addEventListener("click", loadMore)
-
-function getSeach(event) {
-  event.preventDefault()
-  console.log()
-  loadMoreBTN.classList.remove("visually-hidden")
-  if (event.type === "submit") { gallery.innerHTML = "" }
-  console.log(currentSeach)
-  
-        if (currentSeach === form[0].value) {
-          currentPage +=  1
-        } else {currentPage = 1}
-  
+function api() {
   axios.get('https://pixabay.com/api/', {
     params: {
         key: '25440089-75c058e87851521159a5db732',
@@ -36,16 +30,44 @@ function getSeach(event) {
       safesearch: "true",
       per_page: 40,
       page: currentPage
-        
-    }
+            }     
   })
-    
-    
-    .then(data => {
+    .then(async data => {
       currentSeach = form[0].value
-      console.log(currentSeach)
-      markup(data.data.hits)
+      await markup(data.data.hits)
+      
+      if (currentPage !== 1) {
+    scroll()
+      }
+      if (data.data.totalHits === 0) {
+        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+      }
+      else {
+        if (currentPage === 1) {
+          Notiflix.Notify.success(`Hooray!!! We found ${data.data.totalHits} images`)
+        }
+      }
     })
+  ;
+}
+
+form.addEventListener("submit", getSeach)
+window.addEventListener('scroll', throttle(infiniteLoad, 300))
+// loadMoreBTN.addEventListener("click", loadMore)
+
+function getSeach(event) {
+  event.preventDefault()
+  
+  
+  // loadMoreBTN.classList.remove("visually-hidden")
+  if (event.type === "submit") { gallery.innerHTML = "" }
+  
+  
+        if (currentSeach === form[0].value) {
+          currentPage +=  1
+        } else {currentPage = 1}
+  
+  api()
   
 }
  
@@ -80,47 +102,36 @@ function markup(data) {
   
   gallery.insertAdjacentHTML('beforeend', markupData.join(""))
   const lightbox = new SimpleLightbox('.gallery .photo-card a')
-
   
-}
-
-function loadMore() {
-  window.addEventListener('scroll', xcx)
-function xcx(e) { 
-  console.log(document.body.offsetHeight)
-  console.log(window.innerHeight)
-  
-  const scrolled = window.scrollY
-  console.log(scrolled + window.innerHeight)
-  if ((scrolled + window.innerHeight) >= document.body.offsetHeight) {
-     axios.get('https://pixabay.com/api/', {
-    params: {
-        key: '25440089-75c058e87851521159a5db732',
-        q: `${form[0].value}`,
-      image_type: "photo",
-      orientation: "horizontal",
-      safesearch: "true",
-      per_page: 40,
-      page: currentPage
-        
-    }
-  })
-    
-    
-    .then(data => {
-      currentSeach = form[0].value
-      console.log(currentSeach)
-      markup(data.data.hits)
-    })
-
-  }
-
-  // if (document.body.clientHeight ===)
-  
-   
- }
-}
-
-
 
  
+
+  
+}
+
+// function loadMore() {
+//   window.addEventListener('scroll', xcx)
+//   getSeach(event)
+
+// }
+function infiniteLoad(e) {   
+  const scrolled = window.scrollY
+   if ((scrolled + window.innerHeight) >= document.body.offsetHeight - 5) {
+     currentPage += 1
+     console.log(document.querySelector('.gallery').firstElementChild.getBoundingClientRect())
+     api()
+    }
+}
+ 
+function scroll() {
+
+    const { height: cardHeight } = document.querySelector('.gallery').firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+    top: cardHeight * 2 + 30,
+    behavior: 'smooth',
+    })  
+}
+
+
+
+
